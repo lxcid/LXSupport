89dd86963fef381087267a9117ac1cf302f2d4c8
Stan Chang Khin Boon <khinboon@gmail.com>
2012-08-09 00:41:23 +0800

    - Alignment helper methods between 2 rects. Similar to the Align options available in authoring tools like Adobe Flash.

--- src/LXSupport/LXSupport/CGGeometry+LXSupport.h
+++ src/LXSupport/LXSupport/CGGeometry+LXSupport.h
@@ -23,4 +23,40 @@ LXS_CGRectFromCGPointAndCGSize(CGPoint theOrigin, CGSize theSize)
     return CGRectMake(theOrigin.x, theOrigin.y, theSize.width, theSize.height);
 }
 
+CG_INLINE CGFloat
+LXS_CGRectGetAlignedLeftEdgeX(CGRect theRect1, CGRect theRect2)
+{
+    return CGRectGetMinX(theRect1);
+}
+
+CG_INLINE CGFloat
+LXS_CGRectGetAlignedHorizontalCenterX(CGRect theRect1, CGRect theRect2)
+{
+    return CGRectGetMinX(theRect1) + ((CGRectGetWidth(theRect1) - CGRectGetWidth(theRect2)) * 0.5f);
+}
+
+CG_INLINE CGFloat
+LXS_CGRectGetAlignedRightEdgeX(CGRect theRect1, CGRect theRect2)
+{
+    return CGRectGetMaxX(theRect1) - CGRectGetWidth(theRect2);
+}
+
+CG_INLINE CGFloat
+LXS_CGRectGetAlignedTopEdgeY(CGRect theRect1, CGRect theRect2)
+{
+    return CGRectGetMinY(theRect1);
+}
+
+CG_INLINE CGFloat
+LXS_CGRectGetAlignedVerticalCenterY(CGRect theRect1, CGRect theRect2)
+{
+    return CGRectGetMinY(theRect1) + ((CGRectGetHeight(theRect1) - CGRectGetHeight(theRect2)) * 0.5f);
+}
+
+CG_INLINE CGFloat
+LXS_CGRectGetAlignedBottomEdgeY(CGRect theRect1, CGRect theRect2)
+{
+    return CGRectGetMaxY(theRect1) - CGRectGetHeight(theRect2);
+}
+
 #endif /* CGGEOMETRY_LXSUPPORT_H_ */
