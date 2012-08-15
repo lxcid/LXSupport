a9c2f5b1460b60023ccabb23b00e98c32326faf4
Stan Chang Khin Boon <khinboon@gmail.com>
2012-08-15 20:42:20 +0800

    More Test Case to show the power of LXSGraphObject A.K.A FBGraphObject Clone.

--- src/LXSupport/LXSupportTests/LXSGraphObjectTests.m
+++ src/LXSupport/LXSupportTests/LXSGraphObjectTests.m
@@ -9,10 +9,19 @@
 #import "LXSGraphObjectTests.h"
 #import "LXSGraphObject.h"
 
+@protocol LXSPhoneGraphObject <LXSGraphObject>
+
+@property (copy, nonatomic) NSString *manufacturer;
+@property (copy, nonatomic) NSString *model;
+
+@end
+
 @protocol LXSUserGraphObject <LXSGraphObject>
 
 @property (copy, nonatomic) NSString *name;
 @property (strong, nonatomic) NSNumber *age;
+@property (strong, nonatomic) NSMutableDictionary<LXSUserGraphObject> *girlfriend;
+@property (strong, nonatomic) NSMutableDictionary<LXSPhoneGraphObject> *phone;
 
 @end
 
@@ -26,6 +35,21 @@
     STAssertEqualObjects(theUser.name, @"Stan Chang Khin Boon", @"The user's name should be Stan Chang Khin Boon.");
     theUser.age = @26;
     STAssertEqualObjects(theUser.age, @26, @"The user's age should be 26.");
+    
+    STAssertNil(theUser.girlfriend, @"The user's girlfriend should be nil.");
+    theUser.girlfriend = (NSMutableDictionary<LXSUserGraphObject> *)[LXSGraphObject graphObject];
+    STAssertNil(theUser.girlfriend.name, @"The user's girlfriend's name should be nil.");
+    STAssertNil(theUser.girlfriend.age, @"The user's girlfriend's age should be nil.");
+    theUser.girlfriend.name = @"Minty";
+    STAssertEqualObjects(theUser.girlfriend.name, @"Minty", @"The user's girlfriend's name should be Minty.");
+    
+    STAssertNil(theUser.phone, @"The user's phone should be nil.");
+    [theUser
+     setObject:@{ @"manufacturer" : @"Apple" , @"model" : @"iPhone 4S" }
+     forKey:@"phone"];
+    STAssertEqualObjects(theUser.phone.manufacturer, @"Apple", @"The user's phone's manufacturer should be Apple.");
+    STAssertEqualObjects(theUser.phone.model, @"iPhone 4S", @"The user's phone's model should be iPhone 4S.");
+    NSLog(@"%@", theUser);
 }
 
 @end
